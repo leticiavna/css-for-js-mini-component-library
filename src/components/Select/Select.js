8b9709f1437dc221d2edb0691813a12d316b7441
Letícia <leticia@bgcbrasil.com.br>
2023-12-12 19:15:39 -0300

    feat(select): add component styles

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,53 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+const Wrapper = styled.div`
+  width: fit-content;
+  position: relative;
+
+  &:hover > * {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectArrow = styled(Icon)`
+  position: absolute;
+  right: 16px;
+  bottom: 16px;
+  z-index: 1;
+  color: ${COLORS.gray700};
+`;
+
+const SelectBox = styled.select`
+  /* Remove default select arrow */
+  -moz-appearance: none; /* Firefox */
+  -webkit-appearance: none; /* Safari and Chrome */
+  appearance:none;
+
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+  padding: 12px 16px;
+  border-radius: 8px;
+  border: 1px solid transparent;
+  outline-color: ${COLORS.primary};
+  // to check: make this width fluid accordingly to the option selected
+  width: 200px;
+`;
+
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper>
+      <SelectBox value={value} onChange={onChange}>
       {children}
-    </select>
+
+      </SelectBox>
+      <SelectArrow id="chevron-down" size="12" strokeWidth="2" />
+    </Wrapper>
   );
 };
 
