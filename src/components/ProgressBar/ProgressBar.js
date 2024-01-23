b2add36e455d3c978d0375957180416e1c9ca3d8
Letícia Vidal <leticia.vna@gmail.com>
2024-01-23 12:53:55 -0300

    feat(progress-bar): add official solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,29 +5,46 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-
-const SIZES = {
-  small: '8px',
-  medium: '12px',
-  large: '24px',
+/* Create a single styles dict */
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    /* Concentric corners: tweak the outer element to be more curved than the inner one */
+    radius: 8,
+  },
 }
 
 const Wrapper = styled.div`
-  height: ${(props) => SIZES[props.$size]};
-  border-radius: 8px;
   background-color: ${COLORS.transparentGray15};
-  box-shadow: 0px 2px 4px 0px rgba(128, 128, 128, 0.35) inset;
-  padding: ${(props) => props.$size === 'large' ? '4px' : 0};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
 `;
 
 const Bar = styled.div`
-  height: 100%;
+  width: var(--width);
+  height: var(--height);
   background-color: ${COLORS.primary};
-  width: ${(props) => props.$width}%;
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-  border-top-right-radius: ${(props) => props.$width === 100 ? '4px' : 0};
-  border-bottom-right-radius: ${(props) => props.$width === 100 ? '4px' : 0};
+  /* Set the corners clockwise */  
+  border-radius: 4px 0 0 4px;
 `;
 
 /**
@@ -35,13 +52,38 @@ const Bar = styled.div`
  * With this is easier to maintain styles to all browsers and using aria props maintains accessibility
  */
 const ProgressBar = ({ value, size }) => {
+  const styles = STYLES[size];
+
+  /* Nice way to make sure we pass valid prop values (although Typescript can take care of this too) */
+  if (!styles) throw new Error('size not mapped')
+  
   return (
     <Wrapper
       aria-valuenow={value}
-      aria-label="Progress bar"
-      $size={size}
+      /* Accessibility props */
+      aria-valuemin="0"
+      aria-valuemax="100"
+      role="progressbar" 
+      /* Add styles input here as css variables */
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }}
     >
-      <Bar $width={value} $size={size} role="progressbar" />
+      {/* I forgot to add the accessibility in my og solution */}
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      {/**
+        * This needs to exist so we can make sure that the corners are rounding in large size.
+        * Without it, the large size gets funky when near full
+        */}
+      <BarWrapper>
+        <Bar 
+          style={{
+            '--width': value + '%',
+            '--height': styles.height + 'px',
+          }}
+        />
+      </BarWrapper>
     </Wrapper>
   );
 };
